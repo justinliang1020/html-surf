d0c86443d5e14a7ebb49bcb4074b01f0330f3781
Justin Liang <justinliang1020@gmail.com>
2024-07-28 14:34:22 -0400

    refactor to style.css

--- main.go
+++ main.go
@@ -7,10 +7,9 @@ import (
 
 func main() {
 	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static", http.StripPrefix("/static", addCorrectMIMEType(fs)))
-
-	http.HandleFunc("/", IndexHandler)
+	http.Handle("/static/", http.StripPrefix("/static", addCorrectMIMEType(fs)))
 	http.HandleFunc("/editor", EditorHandler)
+	http.HandleFunc("/{$}", IndexHandler)
 
 	log.Println("Starting server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", LoggingMiddleware(http.DefaultServeMux)))
