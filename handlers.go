2b8b36ba507b38301a2ab5e8eab7dedc56b03016
Justin Liang <justinliang1020@gmail.com>
2024-07-28 15:29:53 -0400

    mvp

--- handlers.go
+++ handlers.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"text/template"
 )
@@ -8,7 +10,13 @@ import (
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	if err := templates.ExecuteTemplate(w, "index.html", nil); err != nil {
+	type PageData struct {
+		Pages []string
+	}
+	data := PageData{
+		Pages: htmls,
+	}
+	if err := templates.ExecuteTemplate(w, "index.html", data); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
@@ -17,4 +25,28 @@ func EditorHandler(w http.ResponseWriter, r *http.Request) {
 	if err := templates.ExecuteTemplate(w, "editor.html", nil); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
+}
+
+func PublishHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		log.Println("Error parsing form:", err)
+		http.Error(w, "Error processing request", http.StatusBadRequest)
+		return
+	}
+
+	content := r.Form.Get("content")
+	log.Println("Received content:", content)
+
+	htmls = append(htmls, content)
+	response := `<div>
+        Content published successfully!
+    </div>`
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, response)
 }
