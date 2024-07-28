8affe8b1d4b1f79b2d97c2bd296947f1755ecc6c
Justin Liang <justinliang1020@gmail.com>
2024-07-28 14:05:28 -0400

    index

--- handlers.go
+++ handlers.go
@@ -7,8 +7,14 @@ import (
 
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	if err := templates.ExecuteTemplate(w, "index.html", nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func EditorHandler(w http.ResponseWriter, r *http.Request) {
+	if err := templates.ExecuteTemplate(w, "editor.html", nil); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
--- main.go
+++ main.go
@@ -9,7 +9,8 @@ func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static", http.StripPrefix("/static", addCorrectMIMEType(fs)))
 
-	http.HandleFunc("/", HomeHandler)
+	http.HandleFunc("/", IndexHandler)
+	http.HandleFunc("/editor", EditorHandler)
 
 	log.Println("Starting server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", LoggingMiddleware(http.DefaultServeMux)))
