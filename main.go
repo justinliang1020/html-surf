2ba815d64ffde8481adfa5411e674399a3c79e09
Justin Liang <justinliang1020@gmail.com>
2024-07-29 22:04:51 -0400

    post

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	fs := http.FileServer(http.Dir("static"))
 	http.Handle("/static/", http.StripPrefix("/static", addCorrectMIMEType(fs)))
 	http.HandleFunc("/editor", EditorHandler)
-	http.HandleFunc("/publish", PublishHandler)
+	http.HandleFunc("POST /publish", PublishHandler)
 	http.HandleFunc("/{$}", IndexHandler)
 
 	log.Println("Starting server on :8080")
